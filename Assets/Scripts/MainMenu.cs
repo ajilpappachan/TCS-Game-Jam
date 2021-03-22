8ec1d7f4a1ace68bbcc7339583b5dda8bd73da37
Ajil Pappachan <ajilpappachan@gmail.com>
2021-03-22 16:26:11 +0530

    Created Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject howToPanel;
+
+    public void Play()
+    {
+        LevelManager.Instance.LoadLevel("Game");
+    }
+
+    public void HowTo(bool show)
+    {
+        mainPanel.SetActive(!show);
+        howToPanel.SetActive(show);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
